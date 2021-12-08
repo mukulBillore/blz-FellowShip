c5294905a818adcf2823e89e8d44ac20b3ce1089
mukulBillore <mukulbillore88@gmail.com>
2021-12-08 18:32:58 +0530

    Initial commit sucessusfull

---
+++ Ex.java
@@ -0,0 +1,5 @@
+public class Ex{
+public static void main(String args[]){
+System.out.println("this program is to test the from where the program is stored after each execution in .git folder");
+}
+}
